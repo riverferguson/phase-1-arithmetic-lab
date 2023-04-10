76458cb405dc079ebb13c508e8e2a373160f8b0b
River Ferguson <riverferguson@gmail.com>
2023-04-10 15:08:07 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
+console.log(multiply)
+
+const random = Math.floor(Math.random() * 10) + 1
+console.log(random)
+
+let num3 = 20
+let num4 = 5
+const mod = num3 / num4
+console.log(mod)
+
+
+const max = Math.max(5, 10, 15, 20)
+console.log(max)
